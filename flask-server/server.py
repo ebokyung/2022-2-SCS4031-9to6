297be10d463f1f0f448094fec4c11d41c3ca8f20
0112leesy <0112leesy@naver.com>
2022-10-10 23:26:01 +0900

    0112leesy-10-10-B001-App.js,server.py 코드추가

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,15 +2,19 @@ from flask import Flask # Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+       return "Welcome. Hosting from EC2"
+
 # Users API Route
 
 @app.route('/users')
 def users():
 	# users 데이터를 Json 형식으로 반환한다
-    return {"users": [{ "id" : 1, "name" : "AAAA" },
-    					{ "id" : 2, "name" : "BBBB" },
-                        { "id" : 3, "name" : "CCCC" },
-                        { "id" : 4, "name" : "DDDD" }]}
+    return {"users": [{ "id" : 1, "name" : "AAAA_BE" },
+    					{ "id" : 2, "name" : "BBBB_BE" },
+                        { "id" : 3, "name" : "CCCC_BE" },
+                        { "id" : 4, "name" : "DDDD_BE" }]}
            
 
 if __name__ == "__main__":
