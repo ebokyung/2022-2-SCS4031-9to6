3d1109bbe3e1c191393de7f7c5f3634e111a5c6e
Kiho Kim <kiho7248@nate.com>
2022-10-19 16:13:28 +0900

    kiho7248-10-19-BB001-DB연동 확인 및 스키마변경

--- server/models/member.py
+++ server/models/member.py
@@ -3,6 +3,7 @@
 from models import db
 from datetime import datetime
 
+
 class Member(db.Model):
     id = db.Column(db.String(20), primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
