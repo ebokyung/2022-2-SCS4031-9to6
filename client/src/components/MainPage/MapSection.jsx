d84741fccd9711ed7e48f950c70cb5fce6bbb42d
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-10-20 13:32:36 +0900

    ebokyung-10-20-F005-main page 토글

--- client/src/components/MainPage/MapSection.jsx
+++ client/src/components/MainPage/MapSection.jsx
@@ -1,15 +1,24 @@
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { showSideBar } from '../../atoms';
 
 const Container = styled.section`
     width: 100%;
     max-width: ${props => props.theme.maxWidth};
     min-height: ${props => props.theme.minHeight};
+    padding-left: 460px;
+    transition: all .5s;
+    &.hide {
+        padding-left: 0;
+    }
 `
 
 function MapSection () {
+    const visibility = useRecoilValue(showSideBar);
+
     return(
-    <Container>
-        오른쪽
+    <Container className={visibility ? '' : 'hide'}>
+        {/* ... */}
     </Container>)
 }
 export default MapSection;
--- client/src/components/MainPage/ToggleSection.jsx
+++ client/src/components/MainPage/ToggleSection.jsx
@@ -1,15 +1,46 @@
 import styled from 'styled-components';
+import { useRecoilState } from 'recoil';
+import { showSideBar } from '../../atoms';
 
 const Container = styled.section`
-    width: 36%;
-    max-width: ${props => props.theme.maxWidth};
+    min-width: 460px;
     min-height: ${props => props.theme.minHeight};
+    background-color: #404040;
+    transition: all .5s;
+    position: absolute;
+    left: 0;
+    &.hide {
+        left: -440px;
+    }
+`
+
+const VisibilityToggle = styled.button`
+    width: 30px;
+    height: 40px;
+    position: absolute;
+    left: 460px;
+    transition: all .5s;
+    z-index: 11;
+    border-style: none;
+    background-color: #404040;
 `
 
 function ToggleSection () {
+    
+    const [visibility, setVisibility] = useRecoilState(showSideBar);
+
+    const handleToggle = () => {
+        setVisibility(prev => !prev);
+    }
+
     return(
-    <Container>
-        왼쪽
-    </Container>)
+        <>
+        <Container className={visibility ? '' : 'hide'}>
+            <VisibilityToggle onClick={handleToggle} show={visibility}/>
+            {/* ... */}
+        </Container>
+        
+        </>  
+    )
 }
 export default ToggleSection;
