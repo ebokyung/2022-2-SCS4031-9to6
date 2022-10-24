bc88c7d035cd9b053d244ca37909f69ceaf497ab
Kiho Kim <kiho7248@nate.com>
2022-10-24 19:50:51 +0900

    kiho7248-10-24-B004-회원가입 멤버GET/중복아이디검사

--- server/models/member.py
+++ server/models/member.py
@@ -8,9 +8,9 @@ class Member(db.Model):
     # Member ID
     id = db.Column(db.String(20), primary_key=True)
     # Member Email
-    Email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     # Member Password
-    Password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     
 
 class MemberSchema(ma.SQLAlchemyAutoSchema):
