406c8956bf18f7a7e50501676122484d275b7503
0112leesy <0112leesy@naver.com>
2022-10-25 05:40:48 +0000

    0112leesy-10-25-B005-connect S3

---
+++ server/views/__init__.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+
+s3_config = {
+	# 'aws_access_key_id': os.environ.get('AWS_ACCESS_KEY_ID', 'key'),
+	# 'aws_secret_access_key': os.environ.get('AWS_SECRET_ACCESS_KEY', 'secret'),
+    'aws_access_key_id': 'AKIA3KWLDCIAPZ7J7HE5',
+	'aws_secret_access_key': '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',
+}
+
+def s3_connection():
+    try:
+        s3 = boto3.client(
+            service_name = "s3",
+            region_name = "ap-northeast-2",
+            aws_access_key_id = s3_config['aws_access_key_id'],
+            aws_secret_access_key = s3_config['aws_secret_access_key'],
+        )
+    except Exception as e:
+        print(e)
+    else:
+        print("s3 bucket connected!")
+        return s3
+
+s3 = s3_connection()
---
+++ server/views/historyAPI.py
@@ -0,0 +1,27 @@
+from flask import jsonify
+from flask_restful import Resource
+from models import db
+from models.history import FloodHistory, FloodHistorySchema
+from views import s3
+
+def s3_put_object(s3, bucket, filepath, access_key):
+    """
+    s3 bucket에 지정 파일 업로드
+    :param s3: 연결된 s3 객체(boto3 client)
+    :param bucket: 버킷명
+    :param filepath: 파일 위치
+    :param access_key: 저장 파일명
+    :return: 성공 시 True, 실패 시 False 반환
+    """
+    try:
+        s3.upload_file(
+            Filename=filepath,
+            Bucket=bucket,
+            Key=access_key,
+            ExtraArgs={"ContentType": "image/jpg", "ACL": "public-read"},
+        )
+    except Exception as e:
+        return False
+    return True
+
+s3_put_object(s3, '9to6Bucket', 'flooding.jpg', 'flooding')
