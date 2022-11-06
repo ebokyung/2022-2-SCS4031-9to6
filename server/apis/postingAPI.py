de83ed0f97ba87bfa28ef5819b0a4e6a41385596
0112leesy <0112leesy@naver.com>
2022-11-06 20:26:14 +0900

    0112leesy-11-06-B008-create postingAPI POST method

--- server/apis/postingAPI.py
+++ server/apis/postingAPI.py
@@ -1,7 +1,12 @@
 from flask import jsonify, make_response
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from sqlalchemy.exc import IntegrityError
 from models import db
 from models.posting import Posting, PostingSchema
+from apis import s3
+from datetime import datetime
+import requests, json
+import werkzeug
 
 class Postings(Resource):
     def get(self, posting_index):
@@ -11,6 +16,13 @@ class Postings(Resource):
         return jsonify({'posting' : output})
 
 class PostingList(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('MemberID', type=str, help="Menber ID", location='form')
+    parser.add_argument('Address', required=True, type=str, help="Address", location='form')
+    parser.add_argument('ImageFile', type=werkzeug.datastructures.FileStorage, help="Posting Image File", location='files')
+    parser.add_argument('Content', required=True, type=str, help="Posting Content", location='form')
+
 
     body = ''
     status_code = 501
@@ -22,4 +34,106 @@ class PostingList(Resource):
         self.body = jsonify(output)
         self.status_code = 200
         response = (self.body, self.status_code)
-        return make_response(response) 
+        return make_response(response)
+
+    def post(self):
+        args = self.parser.parse_args()
+
+        memberID = args['MemberID']
+        address = args['Address']
+        imageFile = args['ImageFile']
+        content = args['Content']
+
+        try:
+            filename = imageFile.filename.split('.')[0]
+            ext = imageFile.filename.split('.')[-1]
+            img_name = datetime.now().strftime(f"{filename}-%Y-%m-%d-%H-%M-%S.{ext}")
+
+            s3_put_object(s3, '9to6bucket', imageFile, img_name)
+            image_url = 'https://{bucket_name}.s3.{location}.amazonaws.com/Posting/{s3_path}'.format(bucket_name='9to6bucket', location='ap-northeast-2', s3_path=img_name)
+
+            index = addPosting(memberID, address, content, image_url)
+            query = Posting.query.get(index)
+            schema = PostingSchema()
+            self.body = jsonify(schema.dump(query))
+            self.status_code = 201
+
+        except IntegrityError as error:
+            db.session.rollback()
+
+            error_message = str(error)
+            self.body = jsonify({"error": str(error), "type": "IntegrityError"})
+            if "Duplicate entry" in error_message:
+                self.status_code = 409
+            else:
+                self.status_code = 400
+
+        finally:
+            response = (self.body, self.status_code)
+            response = make_response(response)
+
+        return response
+
+
+def addPosting(memberID, address, content, image_url):
+
+    date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    index = db.session.query(Posting).count() + 1
+    latitude, longitude = get_location(address)
+
+    # 제보 객체 생성
+    posting = Posting(
+        Index = index,
+        MemberID = memberID,
+        Address = address,
+        Datetime = date_time,
+        ImageURL = image_url,
+        Content = content,
+        Longitude = longitude,
+        Latitude = latitude
+    )
+
+    # 테이블에 객체 저장
+    db.session.add(posting)
+    db.session.commit()
+
+    return index
+
+
+# s3에 객체 업로드
+def s3_put_object(s3, bucket, file, filename):
+    """
+    s3 bucket에 지정 파일 업로드
+    :param s3: 연결된 s3 객체(boto3 client)
+    :param bucket: 버킷명
+    :param file: 파일
+    :param filename: 저장 파일명
+    :return: 성공 시 True, 실패 시 False 반환
+    """
+    try:
+        s3.put_object(
+            Body=file,
+            Bucket=bucket,
+            Key=f'Posting/{filename}',
+            ContentType=file.content_type,
+            ACL='public-read',
+            # ExtraArgs={"ContentType": "image/jpg", "ACL": "public-read"},
+        )
+        
+    except Exception as e:
+        print(e)
+        return False
+    return True  
+
+
+def get_location(address):
+  url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + address
+  headers = {"Authorization": "KakaoAK 0db77db2aeaee483b88ae64dd6683afa"}
+  api_json = json.loads(str(requests.get(url,headers=headers).text))
+  # print(api_json)
+  # 주소 검색 결과가 여러 개가 나올 수 있음
+  address = api_json['documents'][0]['address']
+#   crd = {"lat": str(address['y']), "lng": str(address['x'])}
+#   address_name = address['address_name']
+
+  return address['y'], address['x']
